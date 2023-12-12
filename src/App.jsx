cd66dde84c299250d8a8c566105c631350933343
Elmir Abkerimov <abkerimov.e.i20@gmail.com>
2023-12-12 13:45:20 +0300

    7.1 redact

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter, NavLink } from 'react-router-dom';
-import { Routes, Route } from "react-router-dom";
+import {BrowserRouter, NavLink} from 'react-router-dom';
+import {Routes, Route} from "react-router-dom";
 import './App.css';
 import Layout from './Pages/Layout';
 import Home from './Pages/Home';
@@ -10,23 +10,24 @@ import NoPage from './Pages/NoPage';
 import ErrorPage from './Pages/ErrorPage';
 
 const App = () => {
-  return (
-      <BrowserRouter>
-        <nav>
-          <NavLink to="/" exact activeClassName="active">Home</NavLink>
-          <NavLink to="/blogs" activeClassName="active">Blogs</NavLink>
-          <NavLink to="/contact" activeClassName="active">Contact</NavLink>
-        </nav>
-
-        <Routes>
-          <Route path="/" exact element={<Layout><Home /></Layout>} />
-          <Route path="/blogs" element={<Layout><Blogs /></Layout>} />
-          <Route path="/contact" element={<Layout><Contact /></Layout>} />
-          <Route path="*" element={<Layout><NoPage /></Layout>} />
-          <Route path="error" element={<Layout><ErrorPage /></Layout>} />
-        </Routes>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <nav>
+                <NavLink to="/" exact activeClassName="active">Home</NavLink>
+                <NavLink to="/blogs" activeClassName="active">Blogs</NavLink>
+                <NavLink to="/contact" activeClassName="active">Contact</NavLink>
+            </nav>
+            <Layout>
+                <Routes>
+                    <Route path="/" exact element={<Home/>}/>
+                    <Route path="/blogs" element={<Blogs/>}/>
+                    <Route path="/contact" element={<Contact/>}/>
+                    <Route path="*" element={<NoPage/>}/>
+                    <Route path="error" element={<ErrorPage/>}/>
+                </Routes>
+            </Layout>
+        </BrowserRouter>
+    );
 };
 
 export default App;
