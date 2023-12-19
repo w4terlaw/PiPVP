14857eb7ebc7a95f0fec4de747cfa00f8d6a1e73
Elmir Abkerimov <abkerimov.e.i20@gmail.com>
2023-12-19 13:33:42 +0300

    8.2

--- src/ComponentB.js
+++ src/ComponentB.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Context } from "./Context";
 export default function ComponentB() {
-    const [context, setContext] = useContext(Context);
+    const [context, _] = useContext(Context);
     return <div>ComponentB: {context}</div>;
 }
